d7339323024043b8e926253f14776bdc676b147e
Anna Moser <skynet@Annas-Air.fios-router.home>
2019-08-16 11:08:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,36 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0]
+
+cardsInPlay.push('cardOne');
+
+var cardTwo = cards[2];
+
+
+cardsInPlay.push('cardTwo');
+
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		alert("You found a match!");
+	} else {
+  		alert("Sorry, try again.");
+	}	
+
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+// console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardFour);
+
+
